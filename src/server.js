0bd1e7b1f21e576a096cef0ccf05b48110b140dd
yanghajun <hajun.developer@gmail.com>
2023-12-21 21:42:16 +0900

    #16.0(Comment Section) ~ #16.4(API Route part Two)

--- src/server.js
+++ src/server.js
@@ -15,6 +15,7 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 console.log(process.env.COOKIE_SECRET);
 console.log(process.env.DB_URL);
